e815d1af2893dc149448ebdabbec5ba87649ec1b
Tatiyana Galitskaya <tatianagalitskaya3@gmail>
2024-02-20 21:28:57 +0300

    str_func docstring str_title

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def str_func():
-    a = imput()
-    print (return(a.upper()))
+        a = input()
+    print(return(a.upper()))
+
+def str_title():
+    """функция делает заглавными первые буквы каждого слова в строке"""
+    a = input()
+    print(return(a.title()))
